d6df4b61c590e0f401e53673352d72e5073fbcb0
Marina-Syl <marina.bankovskaya18@gmail.com>
2026-02-19 23:57:25 +0200

    edited

--- src/Calculator/calculator.tsx
+++ src/Calculator/calculator.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './calculator.css'
 
-type Operator = "+" | "-" | "*" | "/"
+type Operator = "+" | "-" | "*" | "/" | "%";
 
 export function Calc() {
     const [display, setDisplay] = useState('');
@@ -9,14 +9,14 @@ export function Calc() {
     const [operator, setOperator] = useState<Operator | null>(null);
 
 
-    const numClick = (n: number) => {
+    const numClick = (n: number | string) => {
         setDisplay(display => display + n)
     }
 
     const operatorClick = (oper: string) => {
-        if (!display) return
+        if (!display) return;
 
-        setOperator(oper as Operator)
+        setOperator(oper as Operator);
         setDisplay("")
         setNum1(Number(display))
     }
@@ -24,24 +24,43 @@ export function Calc() {
     const sumClick = () => {
         if (num1 === null || operator === null || !display) {
             console.error("error")
-            console.error(num1,operator,display)
+            console.error(num1, operator, display)
             return
         }
         const num2 = Number(display)
-        let summa= 0
+        let summa = 0
+        switch(operator) {
+            case '+':
+                summa = num1 + num2;
+              break;
+              case '-':
+                summa = num1 - num2;
+              break;
+              case '/':
+                summa = num1 + num2;
+              break;
+              case '*':
+                summa = num1 + num2;
+              break;
+              case '%':
+            //     summa = num1 + num2;
+            //   break;
+            // default:
+              // код, якщо нічого не підійшло
+          }
+        // if (operator === "+") {
+        //     summa = num1 + num2;
+        // }
+        // if (operator === "-") {
+        //     summa = num1 - num2;
+        // }
+        // if (operator === "/") {
+        //     summa = num1 / num2;
+        // }
+        // if (operator === "*") {
+        //     summa = num1 * num2;
+        // }
 
-        if (operator === "+") {
-             summa = num1 + num2
-        }
-        if (operator === "-") {
-            summa = num1 - num2
-        }
-        if (operator === "/") {
-            summa = num1 / num2
-        }
-        if (operator === "*") {
-            summa = num1 * num2   
-        }
         setDisplay(String(summa))
     }
     const resetClick = () => {
@@ -59,24 +78,32 @@ export function Calc() {
                     readOnly>
                 </input>
                 <div className="numbers">
+                    <span onClick={resetClick}>(</span>
+                    <span onClick={resetClick}>)</span>
+                    <span onClick={resetClick}>%</span>
+                    <span onClick={() => operatorClick("/")}>/</span>
+
+                    <span onClick={() => numClick(7)}>7</span>
+                    <span onClick={() => numClick(8)}>8</span>
+                    <span onClick={() => numClick(9)}>9</span>
+                    <span onClick={() => operatorClick("*")}>*</span>
+
+                    <span onClick={() => numClick(4)}>4</span>
+                    <span onClick={() => numClick(5)}>5</span>
+                    <span onClick={() => numClick(6)}>6</span>
+                    <span onClick={() => operatorClick("-")}>-</span>
 
                     <span onClick={() => numClick(1)}>1</span>
                     <span onClick={() => numClick(2)}>2</span>
                     <span onClick={() => numClick(3)}>3</span>
-                    <span onClick={() => numClick(4)}>4</span>
-                    <span onClick={() => numClick(5)}>5</span>
-                    <span onClick={() => numClick(6)}>6</span>
-                    <span onClick={() => numClick(7)}>7</span>
-                    <span onClick={() => numClick(8)}>8</span>
-                    <span onClick={() => numClick(9)}>9</span>
-                    <span onClick={() => numClick(0)}>0</span>
                     <span onClick={() => operatorClick("+")}>+</span>
-                    <span onClick={() => operatorClick("-")}>-</span>
-                    <span onClick={() => operatorClick("/")}>/</span>
-                    <span onClick={() => operatorClick("*")}>*</span>
-                    <span onClick={sumClick}>=</span>
+                   
                     <span onClick={resetClick}>C</span>
-               
+                    <span onClick={() => numClick(0)}>0</span>
+                    <span onClick={() => numClick(".")}>.</span>
+                    <span onClick={sumClick}>=</span>
+
+
 
                 </div>
             </div>
